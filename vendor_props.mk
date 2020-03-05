1bda03aa90cab44df514af33ac2c5b1873a1a853
TheHunter08 <61847975+TheHunter08@users.noreply.github.com>
2020-03-05 20:59:18 +0100

    Add files via upload

---
+++ vendor_props.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=420 \
+    vendor.display.disable_mask_layer_hint=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.release_type=release \
+    ro.vendor.build.real.device=true \
+    vendor.product.device=guacamoleb \
+    vendor.product.manufacturer=oneplus
